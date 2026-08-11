cd2696db3901aac99a305cc753e0f31180692046
Kiszkacy <karol.kiszka@pm.me>
2026-08-11 10:46:25 +0200

    cleanup biomet vars

--- src/modules/biomet.py
+++ src/modules/biomet.py
@@ -102,8 +102,8 @@ SCORE_5_BUTTON: tuple = ui.rect(
     SCORE_BUTTON_HEIGHT
 )
 
-MODE_SWITCH_BUTTON_FILL_COLOR: int = 0x004040 # dark cyan
-MODE_SWITCH_BUTTON_BORDER_COLOR: int = ui.COLOR_WHITE
+BUTTON_FILL_COLOR: int = 0x004040 # dark cyan
+BUTTON_BORDER_COLOR: int = ui.COLOR_WHITE
 BUTTON_TEXT_SIZE: float = 2.0
 BUTTON_TEXT_COLOR: int = ui.COLOR_WHITE
 
@@ -130,17 +130,22 @@ BUTTONS_VISIBLE_WHILE_SCORE_DIALOG_IS_VISIBLE: tuple[tuple, ...] = (
 
 BIOMET_REQUEST_TIMEOUT_MS: int = 1500
 
+IMAGE_STATE_NO_IMAGE: int = 0
+IMAGE_STATE_WAITING: int = 1
+IMAGE_STATE_MISSING: int = 2
+IMAGE_STATE_READY: int = 3
 
+
+_image_state: int = IMAGE_STATE_NO_IMAGE
+_image_bytes: bytes | None = None
+
+current_day_offset: int = 0 # -1 = yesterday, 0 = today, 1 = tomorrow
 _mode: int = 0 # 0 => medical, 1 => personal
+_selected_score: int = -1 # -1 = no score, 1-5 = user's score choice
+
 _score_dialog_visible: bool = False
 _needs_redraw: bool = False
-_selected_score: int = -1 # -1 = no score, 1-5 = user's score choice
 _last_biomet_request_timestamp: int = -1 # for timeout detection
-_waiting_for_image: bool = False
-_received_error_bytes: bool = False
-current_day_offset: int = 0 # -1 = yesterday, 0 = today, 1 = tomorrow
-_image_bytes: bytes | None = None
-_showing_data_missing: bool = False
 
 
 def on_score_received(topic: str, message: bytes):
@@ -157,20 +162,18 @@ def on_score_received(topic: str, message: bytes):
 
 
 def on_image_received(topic: str, message: bytes):
-    global _image_bytes, _needs_redraw, _waiting_for_image, _received_error_bytes, _last_biomet_request_timestamp
+    global _image_bytes, _needs_redraw, _image_state, _last_biomet_request_timestamp
 
-    _waiting_for_image = False
     _last_biomet_request_timestamp = -1
-
+    _needs_redraw = True
     if message.startswith(b"ERROR:"):
         print(f"[BIOMET] Worker failed to provide image: {message.decode()}")
-        _received_error_bytes = True
+        _image_state = IMAGE_STATE_MISSING
         return
 
     print(f"[BIOMET] received biomet image ({len(message)} bytes).")
     _image_bytes = message
-    _received_error_bytes = False
-    _needs_redraw = True
+    _image_state = IMAGE_STATE_READY
 
 
 def request_medical_biomet(day_offset: int = 0):
@@ -190,9 +193,8 @@ def request_personal_biomet(day_offset: int = 0):
 
 
 def request_biomet(day_offset: int = 0):
-    global _waiting_for_image, _received_error_bytes
-    _received_error_bytes = False
-    _waiting_for_image = True
+    global _image_state
+    _image_state = IMAGE_STATE_WAITING
     if _mode == 0:
         request_medical_biomet(day_offset)
     else:
@@ -200,7 +202,7 @@ def request_biomet(day_offset: int = 0):
 
 
 def initialize():
-    global _needs_redraw, _selected_score, _waiting_for_image
+    global _needs_redraw, _selected_score, _image_state, _image_bytes
     display.use_canvas()
 
     mqtt.register_handler(BIOMET__MQTT_IMAGE_RECEIVE_TOPIC, on_image_received)
@@ -211,6 +213,7 @@ def initialize():
 
     _needs_redraw = True
     _selected_score = -1
+    _image_state = IMAGE_STATE_NO_IMAGE
     request_biomet(current_day_offset) # should remember last day and mode that user looked at
     mqtt.send_message(f"{MQTT__WORKER_TOPIC}{BIOMET__MQTT_RATING_REQUEST_WORKER_TOPIC_SUFFIX}", f"{current_day_offset}")
 
@@ -247,28 +250,28 @@ def draw_buttons():
     ui.draw_button(
         MODE_SWITCH_BUTTON,
         label="CUS" if _mode == 0 else "MED",
-        fill_color=BUTTON_DISABLED_FILL_COLOR if disabled else MODE_SWITCH_BUTTON_FILL_COLOR,
-        border_color=BUTTON_DISABLED_BORDER_COLOR if disabled else MODE_SWITCH_BUTTON_BORDER_COLOR,
+        fill_color=BUTTON_DISABLED_FILL_COLOR if disabled else BUTTON_FILL_COLOR,
+        border_color=BUTTON_DISABLED_BORDER_COLOR if disabled else BUTTON_BORDER_COLOR,
         text_color=BUTTON_DISABLED_TEXT_COLOR if disabled else BUTTON_TEXT_COLOR,
         text_size=BUTTON_TEXT_SIZE
     )
 
-    disabled = _score_dialog_visible or current_day_offset >= 6 or _waiting_for_image
+    disabled = _score_dialog_visible or current_day_offset >= 6 or _image_state == IMAGE_STATE_WAITING
     ui.draw_button(
         NEXT_DAY_SWITCH_BUTTON,
         label=">",
-        fill_color=BUTTON_DISABLED_FILL_COLOR if disabled else MODE_SWITCH_BUTTON_FILL_COLOR,
-        border_color=BUTTON_DISABLED_BORDER_COLOR if disabled else MODE_SWITCH_BUTTON_FILL_COLOR,
+        fill_color=BUTTON_DISABLED_FILL_COLOR if disabled else BUTTON_FILL_COLOR,
+        border_color=BUTTON_DISABLED_BORDER_COLOR if disabled else BUTTON_BORDER_COLOR,
         text_color=BUTTON_DISABLED_TEXT_COLOR if disabled else BUTTON_TEXT_COLOR,
         text_size=BUTTON_TEXT_SIZE
     )
 
-    disabled = _score_dialog_visible or current_day_offset <= -6 or _waiting_for_image
+    disabled = _score_dialog_visible or current_day_offset <= -6 or _image_state == IMAGE_STATE_WAITING
     ui.draw_button(
         PREV_DAY_SWITCH_BUTTON,
         label="<",
-        fill_color=BUTTON_DISABLED_FILL_COLOR if disabled else MODE_SWITCH_BUTTON_FILL_COLOR,
-        border_color=BUTTON_DISABLED_BORDER_COLOR if disabled else MODE_SWITCH_BUTTON_FILL_COLOR,
+        fill_color=BUTTON_DISABLED_FILL_COLOR if disabled else BUTTON_FILL_COLOR,
+        border_color=BUTTON_DISABLED_BORDER_COLOR if disabled else BUTTON_BORDER_COLOR,
         text_color=BUTTON_DISABLED_TEXT_COLOR if disabled else BUTTON_TEXT_COLOR,
         text_size=BUTTON_TEXT_SIZE
     )
@@ -291,7 +294,7 @@ def draw_score_dialog():
         SCORE_DIALOG[1],
         SCORE_DIALOG[2],
         SCORE_DIALOG[3],
-        radius=10,
+        radius=8,
         color=SCORE_DIALOG_FILL_COLOR,
         fill=True
     )
@@ -300,7 +303,7 @@ def draw_score_dialog():
         SCORE_DIALOG[1],
         SCORE_DIALOG[2],
         SCORE_DIALOG[3],
-        radius=10,
+        radius=8,
         color=SCORE_DIALOG_BORDER_COLOR
     )
 
@@ -354,12 +357,12 @@ def handle_touch():
             request_biomet(current_day_offset)
             _needs_redraw = True
             mqtt.send_message(f"{MQTT__WORKER_TOPIC}{BIOMET__MQTT_RATING_REQUEST_WORKER_TOPIC_SUFFIX}", f"{current_day_offset}")
-        elif touched_button_index == 1 and touch.was_pressed() and current_day_offset < 6 and not _waiting_for_image: # NEXT_DAY_SWITCH_BUTTON
+        elif touched_button_index == 1 and touch.was_pressed() and current_day_offset < 6 and _image_state != IMAGE_STATE_WAITING: # NEXT_DAY_SWITCH_BUTTON
             current_day_offset += 1
             request_biomet(current_day_offset)
             _needs_redraw = True
             mqtt.send_message(f"{MQTT__WORKER_TOPIC}{BIOMET__MQTT_RATING_REQUEST_WORKER_TOPIC_SUFFIX}", f"{current_day_offset}")
-        elif touched_button_index == 2 and touch.was_pressed() and current_day_offset > -6 and not _waiting_for_image: # PREV_DAY_SWITCH_BUTTON
+        elif touched_button_index == 2 and touch.was_pressed() and current_day_offset > -6 and _image_state != IMAGE_STATE_WAITING: # PREV_DAY_SWITCH_BUTTON
             current_day_offset -= 1
             request_biomet(current_day_offset)
             _needs_redraw = True
@@ -410,24 +413,21 @@ def handle_touch():
 
 
 def update():
-    global _needs_redraw, _image_bytes, _selected_score, current_day_offset, _showing_data_missing, _waiting_for_image
+    global _needs_redraw, _image_bytes, _selected_score, current_day_offset, _image_state
 
     handle_touch()
 
     # if received nodata error flag OR during biomet fetch and didnt receive it yet
-    show_data_missing: bool = False
-    if _received_error_bytes:
-        show_data_missing = True
-    if _waiting_for_image and _last_biomet_request_timestamp != -1:
+    if _image_state == IMAGE_STATE_WAITING and _last_biomet_request_timestamp != -1:
         time_since_request: int = time.ticks_diff(time.ticks_ms(), _last_biomet_request_timestamp)
         if time_since_request > BIOMET_REQUEST_TIMEOUT_MS:
-            show_data_missing = True
-            _waiting_for_image = False
+            _image_state = IMAGE_STATE_MISSING
+            _needs_redraw = True
 
-    if _needs_redraw or (show_data_missing and not _showing_data_missing):
+    if _needs_redraw:
         display.clear_canvas()
 
-        if show_data_missing or _image_bytes is None:
+        if _image_state == IMAGE_STATE_MISSING:
             display.draw_text(
                 "Data missing",
                 x=display.WIDTH // 2,
@@ -436,9 +436,7 @@ def update():
                 anchor=display.MIDDLE_CENTER,
                 color=ui.COLOR_WHITE
             )
-            _showing_data_missing = True
-        else:
-            _showing_data_missing = False
+        elif _image_bytes is not None:
             draw_received_image()
         
         draw_buttons()
